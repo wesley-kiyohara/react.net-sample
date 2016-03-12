e852cef09aeb80a79ff6e2e6b42de122a8086031
unknown <wesley_kiyohara@cz-irv-wks-101.hq.mcwsoftware.local>
2016-03-11 16:26:05 -0800

    moved webpack config; added Dist directory to gitignore

--- react.net-sample/Src/index.js
+++ react.net-sample/Src/index.js
@@ -1,5 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Home = require('./components/Root.react');
+var Root = require('./components/Root.react');
 
 ReactDOM.render(<Root />, document.getElementById('root'));
